d4e9c0068a509873f018d42c5dfc25dc8c5687c7
Amin Achak <82500146+Clancyov@users.noreply.github.com>
2024-04-09 22:13:53 +0330

    Update Python_Script.py
    code implemented

--- Python_Script.py
+++ Python_Script.py
@@ -1,1 +1,16 @@
+import datetime
 
+# Generate a timestamp to use in the filename
+timestamp = datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+
+# Define the content to write to the file
+content = "This is some content that will be written to the file."
+
+# Define the filename using the timestamp
+filename = f"output_{timestamp}.txt"
+
+# Write the content to the file
+with open(filename, "w") as file:
+    file.write(content)
+
+print(f"Content has been written to {filename}")
